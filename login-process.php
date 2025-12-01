3a9785b0fa95b3fc1693a3c552957ad163a6415c
Furairah ZAKARI YAOU IDI <126356918+Furairah3@users.noreply.github.com>
2025-12-01 09:09:32 +0000

    Update login-process.php

--- login-process.php
+++ login-process.php
@@ -25,7 +25,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     } else {
         $_SESSION['error'] = "Invalid email or password!";
-        redirect('index.php');
+        redirect('login.php');
     }
 }
 ?>
