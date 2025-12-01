a0d066846f9714c49e5f0c662a3aea389e98f8b7
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-01 13:00:22 +0000

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,20 @@
-<style>
-    footer {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background-color: #0A2647;
-  color: #EAF6F6;
-  text-align: center;
-  padding: 10px 0;
-  font-size: 13px;
-  z-index: 1000;
-}
-</style>
-<footer>
-    <p>&copy; <?=date('Y')?> Academie Libre Learning Platform — Made with ❤ by Ashesi Students </p>
-</footer>
+    </main>
 
+    <footer>
+        <p>&copy; <?= date('Y') ?> Académie Libre Learning Platform — Made with ❤ by Ashesi Students</p>
+    </footer>
+
+    <style>
+        footer {
+            position: fixed;
+            bottom: 0; left: 0; width: 100%;
+            background-color: #0A2647;
+            color: #EAF6F6;
+            text-align: center;
+            padding: 10px 0;
+            font-size: 13px;
+            z-index: 1000;
+        }
+    </style>
+</body>
 </html>
-
-
